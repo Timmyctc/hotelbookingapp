3fec1791deaa1bc559377ad9e714fef861032be1
Conor Timlin-Canning <c.timlin9@gmail.com>
2024-10-31 11:09:10 +0000

    Adding Customer Class

--- src/main/java/org/dip/tus/Customer.java
+++ src/main/java/org/dip/tus/Customer.java
@@ -1,4 +1,11 @@
 package org.dip.tus;
 
+import java.util.Date;
+
 public class Customer {
+
+    private String name;
+    private Date dateOfBirth;
+    private boolean isPlusMember;
+    //Can be used in conditional logic for calculating customers fees for bookings etc later.
 }
